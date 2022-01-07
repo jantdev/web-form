8450b40b601a41b4f94247b2b2b22fe77364e8dd
Jan T. Jensen <hireme@jantdev.com>
2022-01-07 21:21:51 +0100

    finish

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,28 +1,33 @@
 <template>
-    <form @submit.prevent="handleSubmit">
+    <form @submit.prevent="handleSubmit" ref="theform">
     <label>Email:</label>
     <input type="text" ref="email" v-model="email" @keypress="resetForm">
+    <div class="errorMessage" v-if="error.email">Missing or wrong email address</div>
     <label>Password:</label>
-    <input type="password" ref="password" v-model="password">
+    <input type="password" ref="password" v-model="password" @keypress="resetForm">
+     <div class="errorMessage" v-if="error.password">Missing or wrong password, min 5 letters and numbers</div>
 <label>Role</label>
-<select v-bind="role" ref="role">
-    <option value="0">Select role</option>
+<select v-model="role" ref="role" @click="resetForm">
+    <option disabled value="">Select role</option>
     <option v-for="role in roleOptions" :value="role.value" :key="role.value">{{role.name}}</option> 
 </select>
+ <div class="errorMessage" v-if="error.role">Select a role</div>
 <label>Skills</label>
 <input type="text" ref="skills" placeholder="Enter a skill and press enter" v-model="tempskill" @keydown.enter="editskills">
+ <div class="errorMessage" v-if="error.skills">Add your skills</div>
 <div class="theskills" v-if="showSkills">
     <a href="#" class="skill" v-for="skill in skills" :key="skill" @click="removeskill">{{skill}}</a> 
 </div>
 <div class="terms" ref="terms">
-    <input type="checkbox" v-model="terms">
+    <input type="checkbox" v-model="terms" @click="resetForm">
     <label>Accept terms and conditions</label>
 </div>
+ <div class="errorMessage" v-if="error.terms">You must comply to terms and conditions</div>
 <div class="submit">
     <button>Create an account</button>
 </div>
   </form>
-  <p>{{skills}}</p>
+
 </template>
 
 <script>
@@ -38,28 +43,52 @@ export default {
             tempskill:'',
             skills:[],
             showSkills:false,
+            error:{
+                email:false,
+                password:false,
+                role:false,
+                skills:false,
+                terms:false,
+
+            }
+     
         }
     },
     methods:{
         handleSubmit(){
-            //console.log(this.$refs.email)
-            //this.$refs('email').classList.add('error')
-            //return false
             const Vemail =  /^[\w\.]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+          
             if(!Vemail.test(this.email)){
-                this.$refs.email.classList.add('error')
-                this.$refs.email.focus()
-                
+               this.handleErrors('email')
+            }else if(this.password.length<6){
+               this.handleErrors('password')
+            }else if(!this.role || this.role===0){
+                this.handleErrors('role')
+            }else if(this.skills.length ===0){
+                 this.handleErrors('skills')
+            }else if(!this.terms){
+                 this.handleErrors('terms')
+            }else{
+                const output = 'email: '+this.email+'\npassword: '+this.password+'\nrole: '+this.role+'\nskills: '+this.skills 
+                alert(output)
             }
 
         },
-        editskills(){
+        handleErrors(fieldID){
+             this.$refs[fieldID].classList.add('error')
+                this.$refs[fieldID].focus()
+                this.error[fieldID] = true;
+        },
+
+        editskills(e){
+            e.preventDefault()
             if(this.tempskill==='') return
             let check = this.skills.find((title)=>{
                return title ===this.tempskill
             })
           
            if(check) return
+           this.resetForm()
             this.skills.push(this.tempskill)
             this.tempskill=''
             this.showSkills=true
@@ -74,13 +103,18 @@ export default {
             }    
         },
         resetForm(){
-            let theform = document.querySelector('form');
-         
+            let theform = this.$refs.theform;
+           
             let i=0
             while(i<theform.length){
+                console.log(theform[i])
                 theform[i].classList.remove('error')
                 i++
             }
+            Object.keys(this.error).forEach(key => {
+                this.error[key] = false
+            });
+            this.$refs.terms.classList.remove('error')
         }
     }
 }
@@ -121,7 +155,7 @@ form {
       width:16px;
       display: inline-block;
   }
-  input{
+  input,select{
    outline:none;
   }
   .theskills{
@@ -157,4 +191,10 @@ form {
   .error{
       border:1px solid red;
   }
+  .errorMessage{
+      margin-top:3px;
+      color: red;
+       font-size: 0.9em;
+  }
+  
 </style>
